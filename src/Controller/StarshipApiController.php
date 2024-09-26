0d666e2a8c1aaa247d86d78fa48ce66a7e4c0876
dmicha <d.michalak94@gmail.com>
2024-09-26 15:39:57 +0200

    Change Routing

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,13 +9,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController {
 
-    #[Route('/api/starships')]
+    #[Route('', methods: ['GET'])]
     public function getCollection(LoggerInterface $logger, StarshipRepository $repository): Response {
 
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    function get($id): Response {
+        dd($id);
     }
 }
