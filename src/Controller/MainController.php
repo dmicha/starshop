5a4cd1672239af43a5879234daba263bf101dd7e
dmicha <d.michalak94@gmail.com>
2024-09-27 10:21:03 +0200

    Added new Controller and template, an improve url

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController {
+    
     #[Route('/')]
     public function homepage(StarshipRepository $starshipRepository): Response {
 
